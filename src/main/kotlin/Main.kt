e3a1c69feb16e04304475672da21d296a255920b
Nabifr <Husninabilfareno07@gmail.com>
2023-03-08 19:23:49 +0700

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+fun main() {
+    print("Masukkan angka : ")
+    val number = readLine()?.toIntOrNull()
+
+    if (number == null) {
+        println("Input tidak valid")
+    } else if (isPrima(number)) {
+        println("$number adalah bilangan prima")
+    } else {
+        println("$number bukan bilangan prima")
+    }
+}
+
+fun isPrima(number: Int): Boolean {
+    if (number <= 1) {
+        return false
+    }
+
+    for (i in 2..number / 2) {
+        if (number % i == 0) {
+            return false
+        }
+    }
+
+    return true
+}
